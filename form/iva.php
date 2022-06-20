962b4f8e64aa7b4ebedc50e18c8d971abbddd682
Miguel1 <jmsv2212@gmail.com>
2022-06-20 10:46:52 -0500

    Mi primer commit

---
+++ form/iva.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>If u wanna be my lover</title>
+</head>
+<body>
+    <!-Inicio PHP->
+    <?php
+        $producto=$_GET['producto'];
+        echo $producto;
+    ?>
+</body>
+</html>
